8b0243bda195355c514dbcdc96dd282b3c4d4b6d
andersonconceicao19 <54457571+andersonconceicao19@users.noreply.github.com>
2019-12-06 01:00:29 -0300

    Add files via upload

--- Projeto I/index.js
+++ Projeto I/index.js
@@ -21,6 +21,7 @@ app.get('/', function(req, res){
     Post.findAll().then(function(pos){
         res.render('home', {post: pos})
     })
+
 app.get('/deletar/:id', function(req, res){
     Post.destroy({where: {'id': req.params.id}}).then(function(){
         res.redirect('/')
@@ -39,7 +40,33 @@ app.post('/confirma', function(req, res){
         Conteudo: req.body.conteudo
     }).then(()=> res.redirect('/')).catch((err)=> res.send('erro no: '+ err))
 })
-
+/**ROTA DE EDIÇÃO */
+app.get('/edit/:id', function(req, res){
+    Post.findByPk(req.params.id)
+      .then(post => {
+        res.render('form-edit', {
+          id: req.params.id,
+          titulo: post.titulo,
+          conteudo: post.conteudo
+        })
+      })
+      .catch(err => {
+        res.send('Post não encontrado!')
+      })
+  })
+  app.post('/editado/:id', function(req, res){
+    Post.update({
+      Titulo: req.body.titulo,
+      Conteudo: req.body.conteudo
+    },
+    {
+      where: { id: req.params.id }
+    }).then(function(){
+      res.redirect('/')
+    }).catch(function(err){
+      console.log(err);
+    })
+  })
 
 /**Apontando servidor */
 app.listen(8080, function(){
