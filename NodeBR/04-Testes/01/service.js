efe0eec6fbfad572be8afc5ccf5a6215774267b7
Anderson Conceição <andersonconceiicao@gmail.com>
2021-02-14 19:04:57 -0300

    Passando o primeiro teste

--- NodeBR/04-Testes/01/service.js
+++ NodeBR/04-Testes/01/service.js
@@ -8,7 +8,19 @@ const URL = ' https://swapi.dev/api/people'
 async function obterPessoas(name){
     const url = `${URL}/?search=${name}&format=json`
     const result = await get(url)
-    return result.data
+    return result.data.results.map(mapearPessoas);
+}
+
+/*
+    Método usado para filtrar apenas o que deseja da API
+    No formato correto.
+*/
+ function mapearPessoas(item)
+{
+    return {
+        nome: item.name,
+        peso: item.height
+    }
 }
 
 module.exports = {
--- NodeBR/04-Testes/01/test.js
+++ NodeBR/04-Testes/01/test.js
@@ -1,12 +1,25 @@
+const assert = require('assert')
+const { obterPessoas } = require('./service')
 
-
-// descrevendo o que a switch de teste fará
+/* 
+    * Descrevendo o que a switch de teste fará.
+    * Cada item da switch é representado pelo "IT"
+*/
 describe('Consumindo api heroes', () => {
+    //Primeiro switch 
     it('Buscando o R2D2 no formato correto', async  () => {
-        const expctativa = [{
+        // Resultado esperado da api
+        const expectativa = [{
             nome: 'R2-D2', 
             peso: '96'
         }]
+        // Nome mochado
+        const nomeBase = 'R2-D2'  
+        //buscando o resultado da api, com base no nome mochadk   
+        const resultado = await obterPessoas(nomeBase);
+        
+        // Comparando se o resultado que a api trouxe, é similar ao experado.
+        assert.deepEqual(resultado, expectativa);
         
     })
 })
