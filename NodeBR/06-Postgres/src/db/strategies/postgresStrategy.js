f9c65bad352108682484a8a4c60d415cbc0a0646
Anderson Conceição <andersonconceiicao@gmail.com>
2021-02-17 14:10:01 -0300

    inicio da refatoração da conexão com o banco

--- NodeBR/06-Postgres/src/db/strategies/postgresStrategy.js
+++ NodeBR/06-Postgres/src/db/strategies/postgresStrategy.js
@@ -1,13 +1,68 @@
 const  ICrud  = require("./interfaces/interfaceCrud");
+const Sequelize = require('sequelize');
+
+
 
 class PostgresStrategy extends ICrud {
     constructor(){
         super();
+        this._driver = null;
+        this._herois = null;
+        this.connect();
     }
+    // Método para verificar se o banco está conectado.
     isConnected(){
-
+        try {
+                this._driver.authenticate();
+        } catch (error) {
+            console.log('fail! ',error)
+        }
     }
-    connect()
+
+    // Método para definir o modelo
+
+    defineModel(){
+        this._herois = driver.define('herois', {
+            id: {
+                type: Sequelize.INTEGER,
+                required: true,
+                primaryKey: true,
+                autoIncrement: true
+            }, 
+            nome: {
+                type: Sequelize.STRING,
+                required: true
+            }, 
+            poder: {
+                type: Sequelize.STRING,
+                required: true
+            }
+        },
+        {
+            tableName: 'tb_herois',
+            freezeTableName: false,
+            timestamps: false
+        })
+    
+        this._herois.sync();
+    }
+
+
+    // Método para conexão com o banco de dados, usando drivers
+    connect() {
+        _driver = new Sequelize(
+           'heroes',
+           'anderson',
+           'minhaSenha', {
+               host: 'localhost',
+               dialect: 'postgres',
+               quoteIdentifiers: false,
+               operatorsAliases: false
+           }
+        )        
+    }
+
+    // Método de criação de algo
     create(item) {
        console.log('Olá, Postgres')
     }
--- NodeBR/06-Postgres/src/postgresExample.js
+++ NodeBR/06-Postgres/src/postgresExample.js
@@ -1,40 +1,7 @@
-const Sequelize = require('sequelize');
 
-const driver = new Sequelize(
-    'heroes',
-    'anderson',
-    'minhaSenha', {
-        host: 'localhost',
-        dialect: 'postgres',
-        quoteIdentifiers: false,
-        operatorsAliases: false
-    }
-)
 
 async function main() {
-    const Herois = driver.define('herois', {
-        id: {
-            type: Sequelize.INTEGER,
-            required: true,
-            primaryKey: true,
-            autoIncrement: true
-        }, 
-        nome: {
-            type: Sequelize.STRING,
-            required: true
-        }, 
-        poder: {
-            type: Sequelize.STRING,
-            required: true
-        }
-    },
-    {
-        tableName: 'tb_herois',
-        freezeTableName: false,
-        timestamps: false
-    })
-
-    Herois.sync();
+    
     // await Herois.create({
     //     nome: 'Anderson',
     //     poder: '???I do know ??'
